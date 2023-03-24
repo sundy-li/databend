b17e6a4264f822be30b15e08727e1d432b18305e
sundy-li <543950155@qq.com>
2023-03-24 08:51:34 +0800

    update

--- src/binaries/query/main.rs
+++ src/binaries/query/main.rs
@@ -18,9 +18,13 @@
 mod local;
 
 use std::env;
+use std::sync::Arc;
 
+use common_base::base::tokio;
 use common_base::mem_allocator::GlobalAllocator;
+use common_base::runtime::execute_futures_in_parallel;
 use common_base::runtime::Runtime;
+use common_base::runtime::TrySpawn;
 use common_base::runtime::GLOBAL_MEM_STAT;
 use common_base::set_alloc_error_hook;
 use common_config::InnerConfig;
@@ -30,6 +34,7 @@ use common_exception::ErrorCode;
 use common_exception::Result;
 use common_meta_client::MIN_METASRV_SEMVER;
 use common_metrics::init_default_metrics_recorder;
+use common_storage::DataOperator;
 use common_tracing::set_panic_hook;
 use databend_query::api::HttpService;
 use databend_query::api::RpcService;
@@ -61,6 +66,57 @@ fn main() {
     }
 }
 
+async fn read_segment(data_op: DataOperator, i: i32) -> usize {
+    let index = i % 100;
+    let file_name = format!("tmp/p{index}.txt");
+
+    let time = std::time::Instant::now();
+    let content = data_op.operator().read(&file_name).await.unwrap();
+
+    content.len() as usize
+}
+
+async fn test(thread: usize, permits: usize) -> Result<()> {
+    let runtime = Arc::new(Runtime::with_worker_threads(
+        2,
+        Some("query-ctx".to_string()),
+    )?);
+
+    let ss: Vec<usize> = (0..100000).collect();
+    for chunk in ss.chunks(thread) {
+        let mut chunks = chunk.to_vec();
+        let st = chunks[0];
+        let data_op = common_storage::DataOperator::instance();
+
+        let query_result = runtime.spawn(async move {
+            let mut iter = chunks.iter();
+
+            let futures = std::iter::from_fn(move || {
+                if let Some(location) = iter.next() {
+                    Some(read_segment(data_op.clone(), *location as i32))
+                } else {
+                    None
+                }
+            });
+
+            let results: Vec<usize> = execute_futures_in_parallel(
+                futures,
+                thread,
+                permits,
+                "test-paralleled-futures".to_string(),
+            )
+            .await
+            .unwrap();
+
+            results
+        });
+
+        let query_result = query_result.await.unwrap();
+        println!("read at: {:?} / {}", st, query_result.len());
+    }
+    Ok(())
+}
+
 async fn main_entrypoint() -> Result<()> {
     let conf: InnerConfig = InnerConfig::load()?;
 
@@ -71,244 +127,11 @@ async fn main_entrypoint() -> Result<()> {
     init_default_metrics_recorder();
     set_panic_hook();
     set_alloc_error_hook();
-
-    #[cfg(target_arch = "x86_64")]
-    {
-        if !std::is_x86_feature_detected!("sse4.2") {
-            println!(
-                "Current pre-built binary is typically compiled for x86_64 and leverage SSE 4.2 instruction set, you can build your own binary from source"
-            );
-            return Ok(());
-        }
-    }
-
-    if conf.meta.is_embedded_meta()? {
-        return Err(ErrorCode::Unimplemented(
-            "Embedded meta is an deployment method and will not be supported since March 2023.",
-        ));
-    }
     // Make sure global services have been inited.
     GlobalServices::init(conf.clone()).await?;
 
-    if conf.query.max_memory_limit_enabled {
-        let size = conf.query.max_server_memory_usage as i64;
-        info!("Set memory limit: {}", size);
-        GLOBAL_MEM_STAT.set_limit(size);
-    }
+    let _ = test(16, 16).await;
 
-    let tenant = conf.query.tenant_id.clone();
-    let cluster_id = conf.query.cluster_id.clone();
-    let flight_addr = conf.query.flight_api_address.clone();
-
-    let mut _sentry_guard = None;
-    let bend_sentry_env = env::var("DATABEND_SENTRY_DSN").unwrap_or_else(|_| "".to_string());
-    if !bend_sentry_env.is_empty() {
-        // NOTE: `traces_sample_rate` is 0.0 by default, which disable sentry tracing.
-        let traces_sample_rate = env::var("SENTRY_TRACES_SAMPLE_RATE").ok().map_or(0.0, |s| {
-            s.parse()
-                .unwrap_or_else(|_| panic!("`{}` was defined but could not be parsed", s))
-        });
-
-        _sentry_guard = Some(sentry::init((bend_sentry_env, sentry::ClientOptions {
-            release: common_tracing::databend_semver!(),
-            traces_sample_rate,
-            ..Default::default()
-        })));
-        sentry::configure_scope(|scope| scope.set_tag("tenant", tenant));
-        sentry::configure_scope(|scope| scope.set_tag("cluster_id", cluster_id));
-        sentry::configure_scope(|scope| scope.set_tag("address", flight_addr));
-    }
-
-    #[cfg(not(target_os = "macos"))]
-    check_max_open_files();
-
-    let mut shutdown_handle = ShutdownHandle::create()?;
-
-    info!("Databend Query start with config: {:?}", conf);
-
-    // MySQL handler.
-    {
-        let hostname = conf.query.mysql_handler_host.clone();
-        let listening = format!("{}:{}", hostname, conf.query.mysql_handler_port);
-        let tcp_keepalive_timeout_secs = conf.query.mysql_handler_tcp_keepalive_timeout_secs;
-        let mut handler = MySQLHandler::create(tcp_keepalive_timeout_secs)?;
-        let listening = handler.start(listening.parse()?).await?;
-        shutdown_handle.add_service(handler);
-
-        info!(
-            "Listening for MySQL compatibility protocol: {}, Usage: mysql -uroot -h{} -P{}",
-            listening,
-            listening.ip(),
-            listening.port(),
-        );
-    }
-
-    // ClickHouse HTTP handler.
-    {
-        let hostname = conf.query.clickhouse_http_handler_host.clone();
-        let listening = format!("{}:{}", hostname, conf.query.clickhouse_http_handler_port);
-
-        let mut srv = HttpHandler::create(HttpHandlerKind::Clickhouse);
-        let listening = srv.start(listening.parse()?).await?;
-        shutdown_handle.add_service(srv);
-
-        let http_handler_usage = HttpHandlerKind::Clickhouse.usage(listening);
-        info!(
-            "Listening for ClickHouse compatibility http protocol: {}, Usage: {}",
-            listening, http_handler_usage
-        );
-    }
-
-    // Databend HTTP handler.
-    {
-        let hostname = conf.query.http_handler_host.clone();
-        let listening = format!("{}:{}", hostname, conf.query.http_handler_port);
-
-        let mut srv = HttpHandler::create(HttpHandlerKind::Query);
-        let listening = srv.start(listening.parse()?).await?;
-        shutdown_handle.add_service(srv);
-
-        let http_handler_usage = HttpHandlerKind::Query.usage(listening);
-        info!(
-            "Listening for Databend HTTP API:  {}, Usage: {}",
-            listening, http_handler_usage
-        );
-    }
-
-    // Metric API service.
-    {
-        let address = conf.query.metric_api_address.clone();
-        let mut srv = MetricService::create();
-        let listening = srv.start(address.parse()?).await?;
-        shutdown_handle.add_service(srv);
-        info!("Listening for Metric API: {}/metrics", listening);
-    }
-
-    // Admin HTTP API service.
-    {
-        let address = conf.query.admin_api_address.clone();
-        let mut srv = HttpService::create(&conf);
-        let listening = srv.start(address.parse()?).await?;
-        shutdown_handle.add_service(srv);
-        info!("Listening for Admin HTTP API: {}", listening);
-    }
-
-    // RPC API service.
-    {
-        let address = conf.query.flight_api_address.clone();
-        let mut srv = RpcService::create(conf.clone())?;
-        let listening = srv.start(address.parse()?).await?;
-        shutdown_handle.add_service(srv);
-        info!("Listening for RPC API (interserver): {}", listening);
-    }
-
-    // Cluster register.
-    {
-        ClusterDiscovery::instance()
-            .register_to_metastore(&conf)
-            .await?;
-        info!(
-            "Databend query has been registered:{:?} to metasrv:{:?}.",
-            conf.query.cluster_id, conf.meta.endpoints
-        );
-    }
-
-    // Print information to users.
-    println!("Databend Query");
-    println!();
-    println!("Version: {}", *DATABEND_COMMIT_VERSION);
-    println!();
-    println!("Logging:");
-    println!("    file: {}", conf.log.file);
-    println!("    stderr: {}", conf.log.stderr);
-    println!(
-        "Meta: {}",
-        if conf.meta.is_embedded_meta()? {
-            format!("embedded at {}", conf.meta.embedded_dir)
-        } else {
-            format!("connected to endpoints {:#?}", conf.meta.endpoints)
-        }
-    );
-    println!("Memory:");
-    println!("    limit: {}", {
-        if conf.query.max_memory_limit_enabled {
-            format!(
-                "Memory: server memory limit to {} (bytes)",
-                conf.query.max_server_memory_usage
-            )
-        } else {
-            "unlimited".to_string()
-        }
-    });
-    println!("    allocator: {}", GlobalAllocator::name());
-    println!("    config: {}", GlobalAllocator::conf());
-
-    println!("Cluster: {}", {
-        let cluster = ClusterDiscovery::instance().discover(&conf).await?;
-        let nodes = cluster.nodes.len();
-        if nodes > 1 {
-            format!("[{}] nodes", nodes)
-        } else {
-            "standalone".to_string()
-        }
-    });
-    println!("Storage: {}", conf.storage.params);
-    println!("Cache: {}", conf.cache.data_cache_storage.to_string());
-    println!(
-        "Builtin users: {}",
-        conf.query
-            .idm
-            .users
-            .keys()
-            .map(|name| name.to_string())
-            .collect::<Vec<_>>()
-            .join(", ")
-    );
-    println!();
-    println!("Admin");
-    println!("    listened at {}", conf.query.admin_api_address);
-    println!("MySQL");
-    println!(
-        "    listened at {}:{}",
-        conf.query.mysql_handler_host, conf.query.mysql_handler_port
-    );
-    println!(
-        "    connect via: mysql -uroot -h{} -P{}",
-        conf.query.mysql_handler_host, conf.query.mysql_handler_port
-    );
-    println!("Clickhouse(http)");
-    println!(
-        "    listened at {}:{}",
-        conf.query.clickhouse_http_handler_host, conf.query.clickhouse_http_handler_port
-    );
-    println!(
-        "    usage: {}",
-        HttpHandlerKind::Clickhouse.usage(
-            format!(
-                "{}:{}",
-                conf.query.clickhouse_http_handler_host, conf.query.clickhouse_http_handler_port
-            )
-            .parse()?
-        )
-    );
-    println!("Databend HTTP");
-    println!(
-        "    listened at {}:{}",
-        conf.query.http_handler_host, conf.query.http_handler_port
-    );
-    println!(
-        "    usage: {}",
-        HttpHandlerKind::Query.usage(
-            format!(
-                "{}:{}",
-                conf.query.http_handler_host, conf.query.http_handler_port
-            )
-            .parse()?
-        )
-    );
-
-    info!("Ready for connections.");
-    shutdown_handle.wait_for_termination_request().await;
     info!("Shutdown server.");
     Ok(())
 }
